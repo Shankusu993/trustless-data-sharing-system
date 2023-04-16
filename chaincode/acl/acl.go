9a8f0930446029d99c85f2e271ecc705e5ff8c61
Pankaj <Pankaj08072000@gmail.com>
2023-04-16 14:09:55 +0530

    simplify ACL smartcontract

--- chaincode/acl/acl.go
+++ chaincode/acl/acl.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"time"
-
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 // ACL struct definition
 type ACL struct {
+	Id       	   string    `json:"id"`
 	Identity       string    `json:"identity"`
 	Identifier     string    `json:"identifier"`
 	Qualifier      string    `json:"qualifier"`
@@ -20,93 +20,74 @@ type ACL struct {
 	DisputeSafeguard int      `json:"disputeSafeguard"`
 }
 
-// SimpleChaincode struct definition
-type SimpleChaincode struct {
+// AccessControl contract for managing ACLs
+type AccessControlContract struct {
+	contractapi.Contract
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	return shim.Success(nil)
+// AddACL adds an ACL entry by storing it on the blockchain
+func (ac *AccessControlContract) AddACL(ctx contractapi.TransactionContextInterface, Id, Identity, Identifier, Qualifier) error {
+	acl := ACL{
+		Id:			Id,
+		Identity:	Identity,
+		Identifier:	Identifier,
+		Qualifier:	Qualifier,
+		Validity:	time.Now(), // UTC time
+		Importance:	0,
+		MinBehavior:	0,
+		DisputeSafeguard:	0,
+	}
+
+	aclAsBytes, _ := json.Marshal(acl)
+	return ctx.GetStub().PutState(Id, aclAsBytes)
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
-	if function == "addACL" {
-		return t.addACL(stub, args)
-	} else if function == "getACL" {
-		return t.getACL(stub, args)
+// Updating the ACL on the blockchain
+func (ac *AccessControlContract) UpdateACL(ctx contractapi.TransactionContextInterface, Id, Qualifier string) error {
+	aclAsBytes, err := ctx.GetStub().GetState(Id)
+	if err != nil {
+		return err
 	}
-	return shim.Error("Invalid function name")
+
+	acl := ACL{}
+	err = json.Unmarshal(aclAsBytes, &acl)
+	if err != nil {
+		return err
+	}
+
+	acl.Qualifier = Qualifier
+	acl.Validity = time.Now()
+
+	aclAsBytes, _ = json.Marshal(acl)
+	return ctx.GetStub().PutState(Id, aclAsBytes)
 }
 
-// addACL function definition
-func (t *SimpleChaincode) addACL(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 8 {
-		return shim.Error("Incorrect number of arguments. Expecting 8")
-	}
-	
-	// extract arguments
-	identity := args[0]
-	identifier := args[1]
-	qualifier := args[2]
-	validity, err := strconv.ParseInt(args[3], 10, 64)
+
+// GetACL retrieves an ACL entry from the blockchain
+func (ac *AccessControlContract) GetACL(ctx contractapi.TransactionContextInterface, Id string) (*ACL, error) {
+	aclAsBytes, err := ctx.GetStub().GetState(Id)
 	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to parse validity timestamp: %s", err))
-	}
-	validityTime := time.Unix(validity, 0)
-	importance, err := strconv.Atoi(args[4])
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to parse importance: %s", err))
-	}
-	minBehavior, err := strconv.Atoi(args[5])
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to parse minimum behavior score: %s", err))
-	}
-	disputeSafeguard, err := strconv.Atoi(args[6])
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to parse dispute safeguard: %s", err))
+		return nil, err
 	}
 
-	// create new ACL
-	acl := &ACL{Identity: identity, Identifier: identifier, Qualifier: qualifier, Validity: validityTime,
-			Importance: importance, MinBehavior: minBehavior, DisputeSafeguard: disputeSafeguard}
-	// convert ACL to bytes
-	aclBytes, err := json.Marshal(acl)
+	acl := ACL{}
+	err = json.Unmarshal(aclAsBytes, &acl)
 	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to marshal ACL: %s", err))
+		return nil, err
 	}
 
-	// save ACL to blockchain
-	err = stub.PutState(identity, aclBytes)
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to save ACL: %s", err))
-	}
-
-	return shim.Success(nil)
+	return &acl, nil
 }
 
-// getACL function definition
-func (t *SimpleChaincode) getACL(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-	return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-	// get identity from arguments
-	identity := args[0]
-
-	// get ACL from blockchain
-	aclBytes, err := stub.GetState(identity)
-	if err != nil {
-		return shim.Error(fmt.Sprintf("Failed to get ACL: %s", err))
-	} else if aclBytes == nil {
-		return shim.Error("ACL not found")
-	}
-
-	return shim.Success(aclBytes)
-}
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	chaincode, err := contractapi.NewChaincode(new(AccessControlContract))
 	if err != nil {
-	fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error create access control chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting access control chaincode: %s", err.Error())
 	}
 }
-
